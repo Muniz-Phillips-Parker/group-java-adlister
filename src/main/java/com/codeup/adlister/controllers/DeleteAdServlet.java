f07334d12bd05aa672e3f4f5aff708b13a376b2e
Christian Parker <christianparker512@gmail.com>
2021-02-08 11:00:51 -0600

    just cleaned up the code and got rid of some of the comments and also hid the non-functional delete profile button

--- src/main/java/com/codeup/adlister/controllers/DeleteAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DeleteAdServlet.java
@@ -1,44 +1,4 @@
-//package com.codeup.adlister.controllers;
-//
-//import com.codeup.adlister.dao.DaoFactory;
-//import com.codeup.adlister.models.Ad;
-//import com.codeup.adlister.models.User;
-//
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//@WebServlet(name = "controllers.DeleteAdServlet", urlPatterns = "/delete/*")
-//public class DeleteAdServlet extends HttpServlet {
-////    Ad ad;
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        String selectedAd = request.getPathInfo();
-//        try {
-//            long id = Long.parseLong(request.getParameter("id"));
-//           Ad ad = DaoFactory.getAdsDao().findById(id);
-//            request.setAttribute("id", id);
-//            request.setAttribute("ad", ad);
-//            request.getRequestDispatcher("/WEB-INF/ads/DeleteAd.jsp").forward(request, response);
-//        } catch (NumberFormatException | IndexOutOfBoundsException e) {
-//            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-//        }
-//    }
-//
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        User user = (User) request.getSession().getAttribute("user");
-//        Ad ad = (Ad) request.getSession().getAttribute("ad");
-//
-//        DaoFactory.getAdsDao().deleteAd(ad);
-//        response.sendRedirect("/profile");
-//    }
-//}
-
 package com.codeup.adlister.controllers;
-
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
@@ -55,7 +15,6 @@ import java.io.IOException;
 public class DeleteAdServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //Why grab a query string and substring?
         String query = request.getQueryString();
         Long id = Long.valueOf(query.substring(3));
         DaoFactory.getAdsDao().delete(id);
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -32,22 +32,16 @@
             <h2>${ad.title}</h2>
             <p>${ad.description}</p>
 
-<%--                ATTEMPT TO PULL AD TO EDIT SERVLET--%>
                 <a href="<c:url value="ads/edit" >
     <c:param name="id" value="${ad.id}"/>
     </c:url>">Edit</a>
 
-<%--    /////////////////////////////////////////////////////--%>
-<%--                Deleting the ad feature--%>
-<%--    /////////////////////////////////////////////////////--%>
+<
                 <a href="<c:url value="ads/delete" >
     <c:param name="id" value="${ad.id}"/>
     </c:url>">Delete</a>
 
-<%--                <a class="btn btn-primary deletebtn" id="deletebtn" href="/ads/delete?id=${ad.id}">Delete</a>--%>
 
-<%--                <input type="button" value="Edit" id="Edit" class =" btn btn-secondary" onclick="window.location='ads/edit';">--%>
-<%--                <input type="button" value="Delete" id="Delete" class =" btn btn-secondary">--%>
             </form>
         </div>
     </c:forEach>
@@ -58,7 +52,8 @@
     <div class="container">
         <a href="/updateProfile/${user.id}"><button class="btn btn-primary btn-block">Edit Profile</button></a>
         <br>
-        <a href="delete/${user.id}"><button class="btn btn-primary btn-block">Delete Profile</button></a>
+<%--        Unfortunately didn't get the functionality of the delete user profile to work. Hidding this button--%>
+<%--        <a href="delete/${user.id}"><button class="btn btn-primary btn-block">Delete Profile</button></a>--%>
 
     </div>
 </footer>
